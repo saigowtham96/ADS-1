2b60a2a85a1799346eaa59edd5b04794f5674bdd
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-09-26 23:06:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS 1 Assignments/m3/3-sum/Solution.java
+++ ADS 1 Assignments/m3/3-sum/Solution.java
@@ -9,31 +9,31 @@ public class Solution {
 *
 * @param      args  The arguments
 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int m = s.nextInt();
-		int[] threesum = new int[m];
-		for (int i = 0;i < m;i++) {
-			threesum[i] = s.nextInt();
-		}
-		Arrays.sort(threesum);
-		int count = 0;
-		int j;
-		for(int i = 0;i < m - 2;i++) {
-			j = i+1;
-			int k = m-1;
-			while(j<k) {
-				if(threesum[i] + threesum[j] + threesum[k] == 0) {
-					count++;
-					j++;
-					k--;
-				} else if (threesum[i] + threesum[j] + threesum[k] < 0) {
-					j++;
-				} else {
-					k--;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int[] threesum = new int[m];
+        for (int i = 0;i < m;i++) {
+            threesum[i] = s.nextInt();
+        }
+        Arrays.sort(threesum);
+        int count = 0;
+        int j;
+        for (int i = 0;i < m - 2;i++) {
+            j = i + 1;
+            int k = m - 1;
+            while (j<k) {
+                if(threesum[i] + threesum[j] + threesum[k] == 0) {
+                    count++;
+                    j++;
+                    k--;
+                } else if (threesum[i] + threesum[j] + threesum[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
