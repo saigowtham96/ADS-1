a246768300b2606dc9aca61407f4f9bcc64671bc
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-09-26 23:09:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS 1 Assignments/m3/3-sum/Solution.java
+++ ADS 1 Assignments/m3/3-sum/Solution.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
-/**
+/**.
  * Class for solution.
  */
 public class Solution {
-/**
+/**.
 * { function_description }
 *
 * @param      args  The arguments
 */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         int[] threesum = new int[m];
-        for (int i = 0;i < m;i++) {
+        for (int i = 0; i < m; i++) {
             threesum[i] = s.nextInt();
         }
         Arrays.sort(threesum);
         int count = 0;
         int j;
-        for (int i = 0;i < m - 2;i++) {
+        for (int i = 0; i < m - 2; i++) {
             j = i + 1;
             int k = m - 1;
-            while (j<k) {
-                if(threesum[i] + threesum[j] + threesum[k] == 0) {
+            while (j < k) {
+                if (threesum[i] + threesum[j] + threesum[k] == 0) {
                     count++;
                     j++;
                     k--;
