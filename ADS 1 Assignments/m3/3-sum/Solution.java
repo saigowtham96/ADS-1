dfeface7a6a0722865e2f1b1062a9874d931714d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-09-26 22:55:39 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS 1 Assignments/m3/3-sum/Solution.java
+++ ADS 1 Assignments/m3/3-sum/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
 		}
 		Arrays.sort(threesum);
 		int count = 0;
-		int j =0;
+		int j;
 		for(int i =0;i<m-2;i++) {
-			j = j+1;
+			j = i+1;
 			int k = m-1;
 			while(j<k) {
 				if(threesum[i]+threesum[j]+threesum[k] == 0) {
