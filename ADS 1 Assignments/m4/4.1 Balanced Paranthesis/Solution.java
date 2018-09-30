f70184da96633e76b3fc0f95a40120c123eb0f7d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-10-01 01:46:35 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS 1 Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+/**
+ * { final class }
+ */
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    
+    public static void main(final String[] args) {
+        String input;
+        Scanner scan = new Scanner(System.in);
+        int lines = scan.nextInt();
+        for (int i = 0; i < lines; i++) {
+            BalancedParenthesis obj = new BalancedParenthesis();
+            input = scan.next();
+            obj.add(input);
+            System.out.println(obj.checkInput());
+
+        }
+
+    }
+}
+
+class BalancedParenthesis {
+    /**.
+     * size of the stack array
+     */
+    private int size = 0;
+    /**.
+     * store the open braces
+     */
+    private String[] stack;
+    /**.
+     * store the input elements
+     */
+    private String[] elements;
+    /**.
+     * this method is to add inputs
+     * and perform stack operations on it.
+     *
+     * @param      input  The input
+     */
+    public void add(final String input) {
+        int len = input.length();
+        stack = new String[len];
+        elements = input.split("");
+        for (int i = 0; i < elements.length; i++) {
+            // char a = elements.charAt(i);
+            if (elements[i].equals(
+                        "(") || elements[i].equals(
+                        "[") || elements[i].equals("{")) {
+                push(elements[i]);
+            } else if (elements[i].equals(
+                           ")") || elements[i].equals(
+                           "]") || elements[i].equals("}")) {
+                if (size != 0) {
+                    if (elements[i].equals(")") && (
+                                stack[size - 1].equals(
+                                    "("))) {
+                        pop();
+
+                    } else if (elements[i].equals("]") && (
+                                   stack[size - 1].equals(
+                                       "["))) {
+                        pop();
+                    } else if (elements[i].equals("}") && (
+                                   stack[size - 1].equals(
+                                       ""))) {
+                        pop();
+                    }
+                }
+            }
+        }
+    }
+    /**.
+     * this method adds the element into stack
+     *
+     * @param      element  The element
+     */
+    public void push(final String element) {
+        stack[size++] = element;
+    }
+    
+    public void pop() {
+        size--;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String checkInput() {
+        if (size == 0) {
+            return "YES";
+        }
+        return "NO";
+    }
+
+}
